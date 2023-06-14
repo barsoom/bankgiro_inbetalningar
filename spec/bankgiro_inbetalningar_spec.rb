8910eb05e83f257f1a18f047791b093eb7a4396e
Olle Jonsson <olle.jonsson@auctionet.com>
2023-06-14 10:46:31 +0200

    Nest a spec

--- spec/bankgiro_inbetalningar_spec.rb
+++ spec/bankgiro_inbetalningar_spec.rb
@@ -1,34 +1,36 @@
 require_relative 'spec_helper'
 
-describe BankgiroInbetalningar, ".parse" do
-  context "parsing a minimal file" do
-    subject { BankgiroInbetalningar.parse(fixture_path('minimal.txt')) }
+RSpec.describe BankgiroInbetalningar do
+  describe ".parse" do
+    context "parsing a minimal file" do
+      subject { BankgiroInbetalningar.parse(fixture_path('minimal.txt')) }
 
-    it "finds the timestamp" do
-      subject.timestamp.should == "2004 05 25 173035 010331".gsub(' ','').to_i
-    end
-    it "finds 1 deposit" do
-      subject.deposits.count.should == 1
-    end
-    it "finds 1 payment" do
-      subject.deposits.first.payments.count.should == 1
+      it "finds the timestamp" do
+        subject.timestamp.should == "2004 05 25 173035 010331".gsub(' ','').to_i
+      end
+      it "finds 1 deposit" do
+        subject.deposits.count.should == 1
+      end
+      it "finds 1 payment" do
+        subject.deposits.first.payments.count.should == 1
+      end
     end
   end
-end
 
-describe BankgiroInbetalningar, ".parse_data" do
-  context "parsing a minimal file" do
-    let(:data) { data_from_file('minimal.txt') }
-    subject { BankgiroInbetalningar.parse_data(data) }
+  describe ".parse_data" do
+    context "parsing a minimal file" do
+      let(:data) { data_from_file('minimal.txt') }
+      subject { BankgiroInbetalningar.parse_data(data) }
 
-    it "finds the timestamp" do
-      subject.timestamp.should == "2004 05 25 173035 010331".gsub(' ','').to_i
-    end
-    it "finds 1 deposit" do
-      subject.deposits.count.should == 1
-    end
-    it "finds 1 payment" do
-      subject.deposits.first.payments.count.should == 1
+      it "finds the timestamp" do
+        subject.timestamp.should == "2004 05 25 173035 010331".gsub(' ','').to_i
+      end
+      it "finds 1 deposit" do
+        subject.deposits.count.should == 1
+      end
+      it "finds 1 payment" do
+        subject.deposits.first.payments.count.should == 1
+      end
     end
   end
 end
